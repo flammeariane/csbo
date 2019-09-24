0cbf7278ae86dd33276f67a0d4e5d138d6962883
Balthazar Delvaux <delvaux.balthazar@gmail.com>
2019-09-24 14:57:53 +0200

    form done

--- resources/views/accountEdit.blade.php
+++ resources/views/accountEdit.blade.php
@@ -9,36 +9,41 @@
 
 @section('content')
 
-<form action="">
+<form action="" class="m-4 border border-primary rounded p-4">
     <div class="form-group">
         <label for="nameLabel">Nom</label>
-        <input type="text" class="form-control" id="inputNom" placeholder="Entrez votre nom">
+        <input type="text" class="form-control" id="inputNom" value="Dupont" placeholder="Entrez votre nom">
     </div>
     <div class="form-group">
         <label for="prenomLabel">Prénom</label>
-        <input type="text" class="form-control" id="inputPrenom" placeholder="Entrez votre prénom">
+        <input type="text" class="form-control" id="inputPrenom" value="David" placeholder="Entrez votre prénom">
     </div>
     <div class="form-group">
         <label for="emailLabel">Email</label>
-        <input type="email" class="form-control" id="inputEmail" placeholder="Entrez votre adresse Email">
+        <input type="email" class="form-control" id="inputEmail" value="dupont.david@gmail.com" placeholder="Entrez votre adresse Email">
     </div>
     <div class="form-group">
         <label for="telLabel">Téléphone</label>
-        <input type="number" class="form-control" id="inputTel" placeholder="Entrez votre numéro de téléphone">
+        <input type="number" class="form-control" id="inputTel" value="0473256874" placeholder="Entrez votre numéro de téléphone">
     </div>
     <div class="form-group">
         <label for="dateNaissanceLabel">Date de naissance</label>
-        <input type="date" class="form-control" id="inputNaissance" placeholder="Entrez votre date de naissance">
+        <input type="date" class="form-control" id="inputNaissance" value="12/08/1971" placeholder="Entrez votre date de naissance">
     </div>
     <div class="form-group">
         <label for="adressLabel">Adresse</label>
-        <input type="text" class="form-control" id="inputAdresse1" placeholder="Ligne 1">
-        <input type="text" class="form-control" id="inputAdresse2" placeholder="Ligne 2">
+        <input type="text" class="form-control" id="inputAdresse1" value="15, rue des Granges" placeholder="Ligne 1">
+        <input type="text" class="form-control mt-2" id="inputAdresse2" value="1000 Bruxelles" placeholder="Ligne 2">
     </div>
     <div class="form-group">
-        <label for="matriculeLabel">matricule</label>
-        <input type="text" class="form-control" id="inputMatricule">
+        <label for="matriculeLabel">Matricule</label>
+        <input type="number" class="form-control" value="45821645" id="inputMatricule">
+    </div>
+    <div class="form-group d-flex justify-content-around">
+        <input class="btn btn-primary" type="submit" value="Valider">
+        <input class="btn btn-secondary" type="button" value="Retour">
     </div>
 </form>
+
 
 @endsection
