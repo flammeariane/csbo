5c7c56b4b1c72b38d8c4ebd4a42e3c270ce13300
Balthazar Delvaux <delvaux.balthazar@gmail.com>
2019-09-24 16:16:48 +0200

    account done

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -4,8 +4,23 @@
 
 @section('content')
 
-<h1>My account</h1>
-<a href="/accueil">Back home</a>
+<section>
+    <div class="list-group-flush mt-3">
+        <p class="list-group-item border-0 shadow-sm mx-4 my-2 bg-light">Nom: Dupont</p>
+        <p class="list-group-item border-0 shadow-sm mx-4 my-2 bg-light">Prénom: David</p>
+        <p class="list-group-item border-0 shadow-sm mx-4 my-2 bg-light">Email: dupont.david@gmail.com</p>
+        <p class="list-group-item border-0 shadow-sm mx-4 my-2 bg-light">Téléphone: 0473/25.68.74</p>
+        <p class="list-group-item border-0 shadow-sm mx-4 my-2 bg-light">Date de naissance: 12/08/1971</p>
+        <p class="list-group-item border-0 shadow-sm mx-4 my-2 bg-light">Adresse: 15, rue des Granges 1000 Bruxelles</p>
+        <p class="list-group-item border-0 shadow-sm mx-4 my-2 bg-light">Matricule: 45821645</p>
+    </div>
+    <div class="d-flex justify-content-around">
+        <a href="/accountEdit" class="btn btn-primary">Modifier mes infos</a>
+        <a href="/accueil" class="btn btn-secondary">Retour</a>
+    </div>
+</section>
+
+
 @endsection
 
  
