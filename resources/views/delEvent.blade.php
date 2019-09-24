9e924ae875cb463040cee3f04af0c1e097144e3a
Lesly <Lesly.ghyselinck@gmail.com>
2019-09-24 16:42:21 +0200

    modification de la page delEvent

--- resources/views/delEvent.blade.php
+++ resources/views/delEvent.blade.php
@@ -4,7 +4,8 @@
 
 @section('content')
 
-<section class="container">
+<div class="container">
+
     <div class="evenement">
         <h1>{{__('Evénements')}}</h1>
             <div class="row justify-content-center">
@@ -14,23 +15,26 @@
             </div>
     </div>
 
-    <div id="card" class="card">
-    <h1>{{__('Motif')}}</h1>
-        <div class="card-body">
-            <div class="Motif">
-                <input type="textearea">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div id="motif" class="card">
+                <h1>{{ __('Motif') }}</h1>
+
+                <div class="card-body">
+
+                        <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-">
+                                    <textarea id="motif" name="motif" rows="6"></textarea>
+                                </div>
+                            </div>
+                </div>
             </div>
+            <div class="button-back">
+                <button type="submit" class="btn btn-outline-danger">{{ __('Envoyer') }}</button>
+            </div>
+        </div>
     </div>
-    </div>
-    <div class="bouton-envoyer\ ">
-        <button class="btn btn-outline-danger" type="submit" value="envoyer">Envoyer</button>
-    </div>
-
-    <div class="bouton-retour">
-
-    </div>
-
-</section>
+</div
 
 
 
