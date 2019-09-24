0e126ae815b16af23f45bee3a08a54a4de3c0496
Lesly <Lesly.ghyselinck@gmail.com>
2019-09-24 12:00:08 +0200

    page login terminé

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,7 +26,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-
+                        <div class="row justify-content-center">
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ ('Identifiant') }}" required autocomplete="email" autofocus>
 
@@ -38,6 +38,7 @@
                             </div>
                         </div>
 
+                        <div class="row justify-content-center">
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
@@ -50,18 +51,18 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-danger">
-                                    {{ __('Connexion') }}
-                                </button>
+                                <div class="col-md-8 offset-md-3">
+                                    <button type="submit" class="btn btn-danger">
+                                        {{ __('Connexion') }}
+                                    </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+                                </div>
                             </div>
-                        </div>
                     </form>
                 </div>
             </div>
