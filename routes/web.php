9a357b6d34e4f2ea08ef9477bbeecdd34b694161
Lesly <Lesly.ghyselinck@gmail.com>
2019-09-24 10:50:41 +0200

    mise a jour

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,59 +9,45 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/','Auth\loginController@showLoginFrom');
-
+Route::auth();
+//Route::get('/home', 'HomeController@index');
+Route::get('/','Auth\loginController@showLoginForm');
 Route::get('/cal','PagesController@cal');
-
 Route::get('/calPop','PagesController@calPop');
-
 Route::get('/delEvent','PagesController@delEvent');
-
 Route::get('/account','PagesController@account');
-
+ 
 Route::get('/accountEdit','PagesController@accountEdit');
-
+Route::get('/event', 'PagesController@event');
 Route::get('/myEvent','PagesController@myEvent');
-
 Route::get('/myWear','PagesController@myWear');
-
-
-
-
-
+Route::get('/accueil','PagesController@accueil');
+Route::get('/event','PagesController@event');
 // Route::get('/', function () {
 //     return view('home');
 // });
-
 // Route::get('/calendar', function () {
 //     return view('calendar');
 // });
-
 // Route::get('/calendarPOP', function () {
 //     return view('calendarPOP');
 // });
-
 // Route::get('/deletingEvent', function () {
 //     return view('deletingEvent');
 // });
-
 // Route::get('/myAccount', function () {
 //     return view('myAccount');
 // });
-
 // Route::get('/myAccountEdit', function () {
 //     return view('myAccountEdit');
 // });
-
 // Route::get('/myEvent', function () {
 //     return view('myEvent');
 // });
-
 // Route::get('/myWear', function () {
 //     return view('myWear');
 // });
-
-
-
-
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
