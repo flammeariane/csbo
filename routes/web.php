54cbc93d6b5e6b538675ba3a159edd3a51b830bf
Fouyon Joshua <fouyon.joshua@gmail.com>
2019-10-15 09:31:25 +0200

    Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Auth::routes(['register' => false, 'verify' => false, 'reset' => false, 'remember' => false]);
+Auth::routes(['register' => false, 'verify' => false, 'reset' => false]);
 
 Route::redirect('/', url('login'));
 Route::get('/home', 'HomeController@index')->name('home');
