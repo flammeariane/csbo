708cbadc009c1a047f2479ada45d9f0e60f9e9a0
Lesly <Lesly.ghyselinck@gmail.com>
2019-10-22 16:03:40 +0200

    ajout button

--- resources/views/pages/evenements/show.blade.php
+++ resources/views/pages/evenements/show.blade.php
@@ -8,13 +8,12 @@
                 <div class="card-body">
                     {{ $event->titre }}
                     {{ $event->lieu}} <br>
-                        {{ "Début : ".$event->dateDebut ->format('d/m/y H:i')}}<br>
-                        {{ "Fin : ".$event->dateFin->format('d/m/y H:i')}}
+                    {{ "Début : ".$event->dateDebut ->format('d/m/y H:i')}}<br>
+                    {{ "Fin : ".$event->dateFin->format('d/m/y H:i')}}
                 </div>
                 <div class="card-footer">
                     <div class="float-left">
-                        <button type="button" class="btn btn border-primary"> <a href="mailto:demo@example.com?subject=Proposition - {{ $event->titre }} ({{ $event->dateDebut->format('d/m/Y H:i') }}) - {{ auth()->user()->fullName }}&body=">{{ __('Je me propose') }}</a>
-                        
+                        <button type="button" class="btn btn border-primary"> <a href="mailto:demo@example.com?subject=Proposition - {{ $event->titre }} ({{ $event->dateDebut->format('d/m/Y H:i') }}) - {{ auth()->user()->fullName }}&body=">{{ __('Je me propose') }}</a> </button>
                         <a href="{{ route('events.index') }}" class="btn btn-primary">{{ __('Return back') }}</a>
                     </div>
                 </div>
