811fb46b978d77a997b7721ddaf74745ab6ae8dd
Lesly <Lesly.ghyselinck@gmail.com>
2019-10-22 15:53:07 +0200

    event , ajout lieu, date et bouton je me propose

--- resources/views/pages/evenements/show.blade.php
+++ resources/views/pages/evenements/show.blade.php
@@ -7,9 +7,14 @@
             <div class="card">
                 <div class="card-body">
                     {{ $event->titre }}
+                    {{ $event->lieu}} <br>
+                        {{ "Début : ".$event->dateDebut ->format('d/m/y H:i')}}<br>
+                        {{ "Fin : ".$event->dateFin->format('d/m/y H:i')}}
                 </div>
                 <div class="card-footer">
                     <div class="float-left">
+                        <button type="button" class="btn btn border-primary"> <a href="mailto:demo@example.com?subject=Proposition - {{ $event->titre }} ({{ $event->dateDebut->format('d/m/Y H:i') }}) - {{ auth()->user()->fullName }}&body=">{{ __('Je me propose') }}</a>
+                        
                         <a href="{{ route('events.index') }}" class="btn btn-primary">{{ __('Return back') }}</a>
                     </div>
                 </div>
